d35d95f7e68447cc27d8d69814bfa80fc5ab7e09
Daniel Senften <daniel@senften.org>
2019-03-06 12:10:09 +0100

    Added some more graphics

---
+++ scripts/latex-clean.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+rm -f *.aux
+rm -f *.bbl
+rm -f *.bcf
+rm -f *.blg
+rm -f *.loe
+rm -f *.lof
+rm -f *.log
+rm -f *.lol
+rm -f *.lot
+rm -f *.lox
+rm -f *.nav
+rm -f *.out
+rm -f *.pyg
+rm -f *.snm
+rm -f *.toc
+rm -f *.vrb
+
+rm -f *.gnuplot
+rm -f *.table
+
+rm -rf _minted*
+
+rm -f examples/*.aux
+rm -f examples/*.lof
+rm -f examples/*.log
+rm -f examples/*.lot
+rm -f examples/*.out
+rm -f examples/*.pdf
+rm -f examples/*.toc
+
+rm -f main.article.pdf
+rm -f main.beamer.pdf
